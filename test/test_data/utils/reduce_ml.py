611a84362027e253b29b4e3a220869f2fdd903b5
gvdoord <g.vandenOord@gmail.com>
2021-01-06 13:49:19 +0100

    Fixed syntax error in deprecated utility script

--- test/test_data/utils/reduce_ml.py
+++ test/test_data/utils/reduce_ml.py
@@ -1,58 +1,63 @@
 #!/usr/bin/env python
 
-import traceback
-import sys
-import numpy
+from __future__ import division
+from past.utils import old_div
 import logging
+import traceback
+
+import numpy
 from gribapi import *
 
-VERBOSE=1 # verbose error reporting
+VERBOSE = 1  # verbose error reporting
 log = logging.getLogger(__name__)
 
-def example(ifile,ofile,levels):
+
+def example(ifile, ofile, levels):
     """
     Encoding of the pv coefficients.
     """
     fin = open(ifile)
-    fout = open(ofile,'w')
-    i=0
+    fout = open(ofile, 'w')
+    i = 0
     while True:
         gid = grib_new_from_file(fin)
-        if(not gid): break
-        if(grib_get(gid,"typeOfLevel") == "hybrid"):
-            nlevs = int(grib_get(gid,"numberOfVerticalCoordinateValues"))/2 - 1
-            frac = nlevs/levels
-            if(frac > 0):
-                indices = numpy.arange(nlevs,1,-frac)[::-1]
-                lev = int(grib_get(gid,"level"))
-                code = int(grib_get(gid,"indicatorOfParameter"))
-                if(code not in [134,152]):
-                    if(lev not in indices): continue
+        if not gid: break
+        if grib_get(gid, "typeOfLevel") == "hybrid":
+            nlevs = old_div(int(grib_get(gid, "numberOfVerticalCoordinateValues")), 2) - 1
+            frac = old_div(nlevs, levels)
+            if frac > 0:
+                indices = numpy.arange(nlevs, 1, -frac)[::-1]
+                lev = int(grib_get(gid, "level"))
+                code = int(grib_get(gid, "indicatorOfParameter"))
+                if code not in [134, 152]:
+                    if lev not in indices: continue
                     newlev = indices.tolist().index(lev)
-                    grib_set(gid,"level",newlev)
-                pv = grib_get_array(gid,"pv")
+                    grib_set(gid, "level", newlev)
+                pv = grib_get_array(gid, "pv")
                 newpv = [pv[0]]
                 for i in indices:
-                    newpv.append(pv[i-1])
+                    newpv.append(pv[i - 1])
                 newpv.append(pv[nlevs])
                 for i in indices:
-                    newpv.append(pv[nlevs+i-1])
-                grib_set_array(gid,"pv",newpv)
-                grib_set(gid,"numberOfVerticalCoordinateValues",2*(levels + 1))
-        grib_write(gid,fout)
+                    newpv.append(pv[nlevs + i - 1])
+                grib_set_array(gid, "pv", newpv)
+                grib_set(gid, "numberOfVerticalCoordinateValues", 2 * (levels + 1))
+        grib_write(gid, fout)
         grib_release(gid)
     fout.close()
     fin.close()
 
-def main(ifile,ofile,levels):
+
+def main(ifile, ofile, levels):
     try:
-        example(ifile,ofile,int(levels))
-    except GribInternalError,err:
+        example(ifile, ofile, int(levels))
+    except GribInternalError as err:
         if VERBOSE:
             traceback.print_exc(file=sys.stderr)
         else:
-            log.error(sys.stderr,err.msg)
+            log.error(sys.stderr, err.msg)
         return 1
 
+
 if __name__ == '__main__':
-    sys.exit(main(sys.argv[1],sys.argv[2],sys.argv[3]))
+    sys.exit(main(sys.argv[1], sys.argv[2], sys.argv[3]))
